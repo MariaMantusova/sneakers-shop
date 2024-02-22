9d24b0e2b467d8eef267d1fe5f30b4a71c826253
MariaMantusova <maria.mantusova@mail.ru>
2024-02-22 16:27:20 +0300

    feat: add header

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class='flex justify-between border-b border-slate-300 px-10 py-8'>
+
+    <div class='flex items-center gap-4'>
+      <img class='w-10' src='/logo.png' alt='Логотип сайта'>
+      <div>
+        <h2 class='text-xl font-bold uppercase'>SNEAKERS</h2>
+        <p class='text-slate-400'>Магазин лучших кроссовок</p>
+      </div>
+    </div>
+
+    <ul class='flex items-center gap-10'>
+      <li class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer'>
+        <img src='/cart.svg' alt='Иконка корзинки'>
+        <b>1205 руб.</b>
+      </li>
+      <li class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer'>
+        <img src='/heart.svg' alt='Иконка закладок'>
+        <span>Закладки</span>
+      </li>
+      <li class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer'>
+        <img src='/profile.svg' alt='Иконка профиля'>
+        <span>Профиль</span>
+      </li>
+    </ul>
+
+  </header>
+</template>
+
+<script></script>
