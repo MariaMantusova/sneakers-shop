94b6e14ad8cbeab042ac74645cae5d92fe78bc30
MariaMantusova <maria.mantusova@mail.ru>
2024-02-22 15:52:49 +0300

    g feat: add wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <div>
+    <h1 class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">Мария</h1>
+  </div>
+</template>
 
 <style scoped></style>
