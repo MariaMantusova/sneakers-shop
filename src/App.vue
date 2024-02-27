b1d0361777bce081c6b820e79dee94527b479d59
MariaMantusova <maria.mantusova@mail.ru>
2024-02-27 16:26:49 +0300

    feat: add select on main page

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,23 @@
   <div class='bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14'>
     <Header />
     <section class='p-10'>
-      <div class='flex justify-between'>
+      <div class='flex justify-between items-center'>
         <h2 class='text-3xl font-bold mb-8'>Все кроссовки</h2>
-        <div class='relative'>
-          <img class='absolute left-4 top-3' src='/search.svg' alt='Иконка поиска'/>
-          <input class='border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400'
-            placeholder='Поиск...'
-          />
+
+        <div class='flex gap-4'>
+          <select class='py-2 px-3 border rounded-md outline-none'>
+            <option>По названию</option>
+            <option>По цене (дорогие)</option>
+            <option>По цене (дешевые)</option>
+          </select>
+
+          <div class='relative'>
+            <img class='absolute left-4 top-3' src='/search.svg' alt='Иконка поиска' />
+            <input class='border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400'
+                   placeholder='Поиск...'
+                   type='text'
+            />
+          </div>
         </div>
       </div>
 
