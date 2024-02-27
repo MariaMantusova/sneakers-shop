d1618b8ab5e6e45755619469a38f02a9874e4638
MariaMantusova <maria.mantusova@mail.ru>
2024-02-27 16:18:04 +0300

    feat: add search input on main page

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,16 @@
   <div class='bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14'>
     <Header />
     <section class='p-10'>
-      <h2 class='text-3xl font-bold mb-8'>Все кроссовки</h2>
+      <div class='flex justify-between'>
+        <h2 class='text-3xl font-bold mb-8'>Все кроссовки</h2>
+        <div class='relative'>
+          <img class='absolute left-4 top-3' src='/search.svg' alt='Иконка поиска'/>
+          <input class='border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400'
+            placeholder='Поиск...'
+          />
+        </div>
+      </div>
+
       <CardsList />
     </section>
   </div>
