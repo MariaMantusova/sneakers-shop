6c0b464af4897c4c781d2c582f87d082f36d2197
MariaMantusova <maria.mantusova@mail.ru>
2024-02-28 17:16:17 +0300

    feat: add searching and sorting

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,53 @@
 <script setup>
-  import Header from './components/Header.vue'
-  import CardsList from './components/CardsList.vue'
-  import Drawer from './components/Drawer.vue'
+  import {onMounted, reactive, ref, watch} from 'vue';
+  import axios from 'axios';
+
+  import Header from './components/Header.vue';
+  import CardsList from './components/CardsList.vue';
+  import Drawer from './components/Drawer.vue';
+
+  const items = ref([]);
+
+  const filters = reactive({
+    sortBy: 'title',
+    searchQuery: '',
+  })
+
+  function onChangeSelect(evt) {
+    filters.sortBy = evt.target.value
+  }
+
+  function onChangeSearchInput(evt) {
+    filters.searchQuery = evt.target.value
+  }
+
+  async function fetchItems() {
+    try {
+      const params = {
+        sortBy: filters.sortBy,
+      }
+
+      if (filters.searchQuery) {
+        params.title = `*${filters.searchQuery}*`
+      }
+
+      const { data } = await axios.get('https://604781a0efa572c1.mokky.dev/items', {
+        params
+      });
+
+      items.value = data;
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  onMounted(fetchItems);
+  watch(filters, fetchItems);
+
 </script>
 
 <template>
-<!--  <Drawer />-->
+  <!--  <Drawer />-->
   <div class='bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14'>
     <Header />
     <section class='p-10'>
@@ -13,10 +55,10 @@
         <h2 class='text-3xl font-bold mb-8'>Все кроссовки</h2>
 
         <div class='flex gap-4'>
-          <select class='py-2 px-3 border rounded-md outline-none'>
-            <option>По названию</option>
-            <option>По цене (дорогие)</option>
-            <option>По цене (дешевые)</option>
+          <select @change='onChangeSelect' class='py-2 px-3 border rounded-md outline-none'>
+            <option value='name'>По названию</option>
+            <option value='price'>По цене (дорогие)</option>
+            <option value='-price'>По цене (дешевые)</option>
           </select>
 
           <div class='relative'>
@@ -24,12 +66,13 @@
             <input class='border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400'
                    placeholder='Поиск...'
                    type='text'
+                   @input='onChangeSearchInput'
             />
           </div>
         </div>
       </div>
 
-      <CardsList />
+      <CardsList :items='items' />
     </section>
   </div>
 </template>
--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,11 +1,15 @@
 <script setup>
-  import Card from './Card.vue'
+  import Card from './Card.vue';
+
+  defineProps({
+    items: Array,
+  })
 </script>
 
 <template>
-    <ul class='grid grid-cols-4 gap-5'>
-      <Card imageUrl='/sneakers/sneakers-1.jpg' title='Мужские Кроссовки Nike Blazer Mid Suede'
-            :price='12999' :isFavorite='true' />
+    <ul class='grid grid-cols-4 gap-5 mt-10'>
+      <Card v-for='item in items' :imageUrl='item.imageUrl' :title='item.title'
+            :price='item.price' :key='item.id' />
     </ul>
 </template>
 
