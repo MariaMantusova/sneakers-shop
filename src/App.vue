5cb04996a5972c6db93483f552ad660e6f82564b
MariaMantusova <maria.mantusova@mail.ru>
2024-03-11 17:52:47 +0300

    feat: add home page and vur router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-  import { onMounted, reactive, ref, watch, provide, computed } from 'vue'
+  import { ref, watch, provide, computed } from 'vue';
   import axios from 'axios'
 
   import Header from './components/Header.vue';
-  import CardsList from './components/CardsList.vue';
   import Drawer from './components/Drawer.vue';
+  import Home from './pages/Home.vue';
 
-  const items = ref([]);
   const drawer = ref([]);
   const drawerOpen = ref(false);
   const isCreatingOrder = ref(false);
@@ -15,11 +14,6 @@
   const totalPrice = computed(() => drawer.value.reduce((acc, item) => acc + item.price, 0));
   const vatPrice = computed(() => Math.round((totalPrice.value * 5) / 100));
   const drawerButtonDisabled = computed(() => isCreatingOrder.value || drawerIsEmpty.value);
-
-  const filters = reactive({
-    sortBy: 'title',
-    searchQuery: ''
-  })
 
   function closeDrawer() {
     drawerOpen.value = false
@@ -37,92 +31,6 @@
   function removeFromCart(item) {
     drawer.value.splice(drawer.value.indexOf(item), 1);
     item.isAdded = false
-  }
-
-  function onClickAddToCart(item) {
-    if (!item.isAdded) {
-      addToCart(item)
-    } else {
-      removeFromCart(item)
-    }
-  }
-
-  function onChangeSelect(evt) {
-    filters.sortBy = evt.target.value
-  }
-
-  function onChangeSearchInput(evt) {
-    filters.searchQuery = evt.target.value
-  }
-
-  async function fetchFavorites() {
-    try {
-      const { data: favorites } = await axios.get('https://4860d1de94ba74d5.mokky.dev/favorites')
-
-      items.value = items.value.map(item => {
-        const favoriteItem = favorites.find(favorite => favorite.parentId === item.id)
-
-        if (!favoriteItem) {
-          return item
-        }
-
-        return {
-          ...item,
-          isFavorite: true,
-          favoriteId: favoriteItem.id
-        }
-      })
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  async function addToFavorites(item) {
-    try {
-      if (!item.isFavorite) {
-        const obj = {
-          parentId: item.id
-        }
-        item.isFavorite = true
-
-        const { data } = await axios.post('https://4860d1de94ba74d5.mokky.dev/favorites', obj)
-
-        item.favoriteId = data.id
-      } else {
-        item.isFavorite = false
-        await axios.delete(`https://4860d1de94ba74d5.mokky.dev/favorites/${item.favoriteId}`)
-        item.favoriteId = null
-      }
-
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  async function fetchItems() {
-    try {
-      const params = {
-        sortBy: filters.sortBy
-      }
-
-      if (filters.searchQuery) {
-        params.title = `*${filters.searchQuery}*`
-      }
-
-      const { data } = await axios.get('https://4860d1de94ba74d5.mokky.dev/items', {
-        params
-      })
-
-      items.value = data.map((obj) => ({
-        ...obj,
-        isFavorite: false,
-        favoriteId: null,
-        isAdded: false
-      }))
-
-    } catch (e) {
-      console.log(e)
-    }
   }
 
   async function createOrder() {
@@ -143,28 +51,6 @@
     }
   }
 
-  onMounted(async () => {
-    const localDrawer = localStorage.getItem('drawer');
-    drawer.value = localDrawer ? JSON.parse(localDrawer) : [];
-
-    await fetchItems();
-    await fetchFavorites();
-
-    items.value = items.value.map((item) => ({
-      ...item,
-      isAdded: drawer.value.some((drawerItem) => drawerItem.id === item.id)
-    }))
-  })
-
-  watch(filters, fetchItems);
-
-  watch(drawer, () => {
-    items.value = items.value.map((item) => ({
-      ...item,
-      isAdded: false
-    }))
-  });
-
   watch(drawer, () => {
     localStorage.setItem('drawer', JSON.stringify(drawer.value));
   }, {deep: true})
@@ -173,6 +59,7 @@
     drawer,
     closeDrawer,
     openDrawer,
+    addToCart,
     removeFromCart
   })
 </script>
@@ -183,28 +70,7 @@
   <div class='bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14'>
     <Header :total-price='totalPrice' @open-drawer='openDrawer' />
     <section class='p-10'>
-      <div class='flex justify-between items-center'>
-        <h2 class='text-3xl font-bold mb-8'>Все кроссовки</h2>
-
-        <div class='flex gap-4'>
-          <select @change='onChangeSelect' class='py-2 px-3 border rounded-md outline-none'>
-            <option value='name'>По названию</option>
-            <option value='price'>По цене (дорогие)</option>
-            <option value='-price'>По цене (дешевые)</option>
-          </select>
-
-          <div class='relative'>
-            <img class='absolute left-4 top-3' src='/search.svg' alt='Иконка поиска' />
-            <input class='border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400'
-                   placeholder='Поиск...'
-                   type='text'
-                   @input='onChangeSearchInput'
-            />
-          </div>
-        </div>
-      </div>
-
-      <CardsList :items='items' @add-to-favorite='addToFavorites' @add-to-cart='onClickAddToCart' />
+      <Home />
     </section>
   </div>
 </template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,148 @@
+<script setup>
+  import axios from 'axios';
+  import { inject, onMounted, reactive, ref, watch } from 'vue'
+  import CardsList from '../components/CardsList.vue';
+
+  const items = ref([]);
+
+  const { drawer, addToCart, removeFromCart} = inject("drawer");
+
+  const filters = reactive({
+    sortBy: 'title',
+    searchQuery: ''
+  })
+
+  function onChangeSelect(evt) {
+    filters.sortBy = evt.target.value
+  }
+
+  function onClickAddToCart(item) {
+    if (!item.isAdded) {
+      addToCart(item)
+    } else {
+      removeFromCart(item)
+    }
+  }
+
+  function onChangeSearchInput(evt) {
+    filters.searchQuery = evt.target.value
+  }
+
+  async function addToFavorites(item) {
+    try {
+      if (!item.isFavorite) {
+        const obj = {
+          parentId: item.id
+        }
+        item.isFavorite = true
+
+        const { data } = await axios.post('https://4860d1de94ba74d5.mokky.dev/favorites', obj)
+
+        item.favoriteId = data.id
+      } else {
+        item.isFavorite = false
+        await axios.delete(`https://4860d1de94ba74d5.mokky.dev/favorites/${item.favoriteId}`)
+        item.favoriteId = null
+      }
+
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  async function fetchFavorites() {
+    try {
+      const { data: favorites } = await axios.get('https://4860d1de94ba74d5.mokky.dev/favorites')
+
+      items.value = items.value.map(item => {
+        const favoriteItem = favorites.find(favorite => favorite.parentId === item.id)
+
+        if (!favoriteItem) {
+          return item
+        }
+
+        return {
+          ...item,
+          isFavorite: true,
+          favoriteId: favoriteItem.id
+        }
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  async function fetchItems() {
+    try {
+      const params = {
+        sortBy: filters.sortBy
+      }
+
+      if (filters.searchQuery) {
+        params.title = `*${filters.searchQuery}*`
+      }
+
+      const { data } = await axios.get('https://4860d1de94ba74d5.mokky.dev/items', {
+        params
+      })
+
+      items.value = data.map((obj) => ({
+        ...obj,
+        isFavorite: false,
+        favoriteId: null,
+        isAdded: false
+      }))
+
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  onMounted(async () => {
+    const localDrawer = localStorage.getItem('drawer');
+    drawer.value = localDrawer ? JSON.parse(localDrawer) : [];
+
+    await fetchItems();
+    await fetchFavorites();
+
+    items.value = items.value.map((item) => ({
+      ...item,
+      isAdded: drawer.value.some((drawerItem) => drawerItem.id === item.id)
+    }))
+  })
+
+  watch(filters, fetchItems);
+
+  watch(drawer, () => {
+    items.value = items.value.map((item) => ({
+      ...item,
+      isAdded: false
+    }))
+  });
+</script>
+
+<template>
+  <div class='flex justify-between items-center'>
+    <h2 class='text-3xl font-bold mb-8'>Все кроссовки</h2>
+
+    <div class='flex gap-4'>
+      <select @change='onChangeSelect' class='py-2 px-3 border rounded-md outline-none'>
+        <option value='name'>По названию</option>
+        <option value='price'>По цене (дорогие)</option>
+        <option value='-price'>По цене (дешевые)</option>
+      </select>
+
+      <div class='relative'>
+        <img class='absolute left-4 top-3' src='/search.svg' alt='Иконка поиска' />
+        <input class='border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400'
+               placeholder='Поиск...'
+               type='text'
+               @input='onChangeSearchInput'
+        />
+      </div>
+    </div>
+  </div>
+
+  <CardsList :items='items' @add-to-favorite='addToFavorites' @add-to-cart='onClickAddToCart' />
+</template>
+
