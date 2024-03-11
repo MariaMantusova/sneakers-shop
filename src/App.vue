4532e3cf93a2434551f96778fe993ed966adbac5
MariaMantusova <maria.mantusova@mail.ru>
2024-03-11 18:07:23 +0300

    feat: add routing

--- src/App.vue
+++ src/App.vue
@@ -70,7 +70,7 @@
   <div class='bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14'>
     <Header :total-price='totalPrice' @open-drawer='openDrawer' />
     <section class='p-10'>
-      <Home />
+      <router-view></router-view>
     </section>
   </div>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,22 +10,26 @@
 <template>
   <header class='flex justify-between border-b border-slate-200 px-10 py-8'>
 
-    <div class='flex items-center gap-4'>
-      <img class='w-10' src='/logo.png' alt='Логотип сайта'>
-      <div>
-        <h1 class='text-xl font-bold uppercase'>SNEAKERS</h1>
-        <p class='text-slate-400'>Магазин лучших кроссовок</p>
+    <router-link to="/">
+      <div class='flex items-center gap-4'>
+        <img class='w-10' src='/logo.png' alt='Логотип сайта'>
+        <div>
+          <h1 class='text-xl font-bold uppercase'>SNEAKERS</h1>
+          <p class='text-slate-400'>Магазин лучших кроссовок</p>
+        </div>
       </div>
-    </div>
+    </router-link>
 
     <ul class='flex items-center gap-10'>
       <li class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer' @click='() => emit("openDrawer")'>
         <img src='/cart.svg' alt='Иконка корзинки'>
         <b>{{ totalPrice }} руб.</b>
       </li>
-      <li class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer'>
-        <img src='/heart.svg' alt='Иконка закладок'>
-        <span>Закладки</span>
+      <li>
+        <router-link to='/favorites' class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer'>
+          <img src='/heart.svg' alt='Иконка закладок'>
+          <span>Закладки</span>
+        </router-link>
       </li>
       <li class='flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer'>
         <img src='/profile.svg' alt='Иконка профиля'>
